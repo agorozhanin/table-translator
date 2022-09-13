e9e1b3cfb30a06012e9ab552b9e3f6c2c287e5cc
Alex <al.gorzhanin23@gmail.com>
2022-09-13 20:42:50 +0300

    Create base models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Declaration(models.Model):
+    """
+    Модель декларации. Содержит атрибуты:
+    id - первичный ключ записи (int)
+    order_num - номер заказа (int)
+    price_in_dollars - цена в долларах (decimal)
+    price_in_roubles - цена в рублях (decimal)
+    delivery_time - срок поставки (date)
+    """
+
+    class Meta:
+        verbose_name = "Декларация"
+        verbose_name_plural = "Декларации"
+
+    id = models.AutoField(verbose_name="ID", primary_key=True)
+    order_num = models.IntegerField(verbose_name="Номер заказа", null=False, unique=True, blank=False)
+    price_in_dollars = models.DecimalField(verbose_name="Стоимость в долларах", null=False, unique=False, blank=False,
+                                           max_digits=9, decimal_places=3)
+    price_in_roubles = models.DecimalField(verbose_name="Стоимость в рублях", null=False, unique=False, blank=False,
+                                           max_digits=9, decimal_places=3)
+    delivery_time = models.DateField(verbose_name="Срок поставки", null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.id}. {self.order_num}"
--- table_translator/settings.py
+++ table_translator/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'table_translator.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'table_translator_db',
+        'USER': 'postgres',
+        'PASSWORD': '111',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
