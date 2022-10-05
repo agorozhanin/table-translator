c8916f4a1169630c6017720f06e460b677d060f4
Alexandr <al.gorozhanin23@gmail.com>
2022-10-05 10:22:00 +0300

    Create interface

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from main.views import DeclarationApiView
+from main.views import DeclarationApiView, index, update_table
 
 urlpatterns = [
-    path('api/save-declarations', DeclarationApiView.as_view())
+    path('api/save-declarations', DeclarationApiView.as_view()),
+    path('', index, name = 'index'),
+    path('update_table', update_table, name='update_table'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render, redirect
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -9,6 +10,11 @@ from main.service.declaration.use_cases import insert_declaration_to_table
 from main.service.google_sheets.google_sheets_service import get_data_from_googlesheets
 
 
+def index(request):
+    rows = Declaration.objects.all()
+    return render(request, 'main/index.html', {'rows': rows})
+
+
 class DeclarationApiView(APIView):
     serializer_class = DeclarationModelSerializer
 
@@ -16,3 +22,9 @@ class DeclarationApiView(APIView):
         table_from_google = get_data_from_googlesheets()
         insert_declaration_to_table(table_from_google)
         return Response(self.serializer_class(get_all_declarations(), many=True).data, status=status.HTTP_200_OK)
+
+
+def update_table(request):
+    table_from_google = get_data_from_googlesheets()
+    insert_declaration_to_table(table_from_google)
+    return redirect('index')
